c9610c8eb9aeecf9b809f215aafbb96994e2a896
olehzherebukh <olehzherebukh@gmail.com>
2019-02-11 18:16:01 +0200

    deleted needless lines

--- src/project1/Main.java
+++ src/project1/Main.java
@@ -6,14 +6,12 @@ public class Main {
 		SwimmingPool pool1 = new SwimmingPool();
 		SwimmingPool pool2 = new SwimmingPool(3, "Peter", 40, 45);
 		SwimmingPool pool3 = new SwimmingPool(5, "Michael", 6000, 50, "Kopernyka str.", 40, "Oleh");
-		SwimmingPool pool4 = new SwimmingPool();
 		
 		System.out.println(pool1.toString());
 		System.out.println(pool2.toString());
 		System.out.println(pool3.toString());
+		
 		SwimmingPool.printStaticNumberOfVisitorsToday​();
-		pool4.resetValues(4, "Maria", 3, 24, "Academic str.", 35, "Max");
-		System.out.println(pool4);
 	}
 
 }
