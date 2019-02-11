28384892f03d39ab92f81b277eed9be6939e2724
olehzherebukh <olehzherebukh@gmail.com>
2019-02-11 18:14:06 +0200

    fixed and cleancoded code

--- src/project1/Main.java
+++ src/project1/Main.java
@@ -3,14 +3,17 @@ package project1;
 public class Main {
 
 	public static void main(String[] args) {
-		SwimmingPool obj1 = new SwimmingPool();
-		SwimmingPool obj2 = new SwimmingPool(3, "Peter", 40, 45);
-		SwimmingPool obj3 = new SwimmingPool(5, "Michael", 6000, 50, "Kopernyka str.", 40, "Oleh");
+		SwimmingPool pool1 = new SwimmingPool();
+		SwimmingPool pool2 = new SwimmingPool(3, "Peter", 40, 45);
+		SwimmingPool pool3 = new SwimmingPool(5, "Michael", 6000, 50, "Kopernyka str.", 40, "Oleh");
+		SwimmingPool pool4 = new SwimmingPool();
 		
-		System.out.println(obj1.toString());
-		System.out.println(obj2.toString());
-		System.out.println(obj3.toString());
+		System.out.println(pool1.toString());
+		System.out.println(pool2.toString());
+		System.out.println(pool3.toString());
 		SwimmingPool.printStaticNumberOfVisitorsToday​();
+		pool4.resetValues(4, "Maria", 3, 24, "Academic str.", 35, "Max");
+		System.out.println(pool4);
 	}
 
 }
--- src/project1/SwimmingPool.java
+++ src/project1/SwimmingPool.java
@@ -1,32 +1,56 @@
 package project1;
 
 public class SwimmingPool {
-	public String Address;
-	public int Volume;
-	public int MaxAmoutOfVisitors;
+	protected int width;
+	protected String nameOfVisitor;
 	
-	private int Depth;
-	private String NameOfCoach;
+	private String address;
+	private int volume;
+	private int maxAmoutOfVisitors;
+	private int depth;
+	private String nameOfCoach;
 	 
 	public static int NumberOfVisitorsToday = 28;
 	
-	protected int Width;
-	protected String NameOfVisitor;
 	
+	public String getAddress() {
+        return this.address;
+    }
+	
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    public int getVolume() {
+        return this.volume;
+    }
+	
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+    
+    public int getMaxAmoutOfVisitors() {
+        return this.maxAmoutOfVisitors;
+    }
+	
+    public void setMaxAmoutOfVisitors(int maxAmoutOfVisitors) {
+        this.maxAmoutOfVisitors = maxAmoutOfVisitors;
+    }
 	
 	public int getDepth() {
-        return this.Depth;
+        return this.depth;
     }
+	
     public void setDepth(int depth) {
-        this.Depth = depth;
+        this.depth = depth;
     }
- 
  
     public String getNameOfCoach() {
-        return this.NameOfCoach;
+        return this.nameOfCoach;
     }
+    
     public void setNameOfCoach(String nameOfCoach) {
-        this.NameOfCoach = nameOfCoach ;
+        this.nameOfCoach = nameOfCoach ;
     }
     
     
@@ -34,47 +58,55 @@ public class SwimmingPool {
     	
     }
     
-    public SwimmingPool(int dep, String name, int vol, int amountOfVis) {
-    	this.Depth = dep;
-    	this.NameOfCoach = name;
-    	this.Volume = vol;
-    	this.MaxAmoutOfVisitors = amountOfVis;
+    public SwimmingPool(int dep, String name,
+    		int vol, int amountOfVis) {
+    	this.depth = dep;
+    	this.nameOfCoach = name;
+    	this.volume = vol;
+    	this.maxAmoutOfVisitors = amountOfVis;
     }
     
-    public SwimmingPool(int dep, String name, int vol, int amountOfVis, String adr, int wid, String nameOfVis) {
+    public SwimmingPool(int dep, String name,
+    		int vol, int amountOfVis,
+    		String adr, int wid,
+    		String nameOfVis) {
     	this (dep, name, vol, amountOfVis);
-    	this.Address = adr;
-    	this.Width = wid;
-    	this.NameOfVisitor = nameOfVis;
+    	this.address = adr;
+    	this.width = wid;
+    	this.nameOfVisitor = nameOfVis;
     }
     
     public String toString() {
     	String GeneralInfo = "\t" + "General Info about Swimming Pool" + "\t" + "\n";
-    	GeneralInfo = GeneralInfo + "Address:" + Address + "\n";
-    	GeneralInfo = GeneralInfo + "Volume:" + Volume + "\n";
-    	GeneralInfo = GeneralInfo + "Max amout of visitors:" + MaxAmoutOfVisitors + "\n";
-    	GeneralInfo = GeneralInfo + "Depth:" + Depth + "\n";
-    	GeneralInfo = GeneralInfo + "Name of coach:" + NameOfCoach + "\n";
-    	GeneralInfo = GeneralInfo + "Width:" + Width + "\n";
-    	GeneralInfo = GeneralInfo + "Name of visitor:" + NameOfVisitor + "\n";
+    	GeneralInfo = GeneralInfo + "Address:" + address + "\n";
+    	GeneralInfo = GeneralInfo + "Volume:" + volume + "\n";
+    	GeneralInfo = GeneralInfo + "Max amout of visitors:" + maxAmoutOfVisitors + "\n";
+    	GeneralInfo = GeneralInfo + "Depth:" + depth + "\n";
+    	GeneralInfo = GeneralInfo + "Name of coach:" + nameOfCoach + "\n";
+    	GeneralInfo = GeneralInfo + "Width:" + width + "\n";
+    	GeneralInfo = GeneralInfo + "Name of visitor:" + nameOfVisitor + "\n";
     	return GeneralInfo;
     }
     
     public static void printStaticNumberOfVisitorsToday​() {
-    	System.out.println(NumberOfVisitorsToday);
+    	System.out.println("Number Of Visitors Today:" +NumberOfVisitorsToday + "\n");
     }
     
     public void printNumberOfVisitorsToday​() {
-    	System.out.println(NumberOfVisitorsToday);
+    	System.out.println("Number Of Visitors Today:" +NumberOfVisitorsToday + "\n");
     }
     
-    public void resetValues(int dep, String name, int vol, int amountOfVis, String adr, int wid, String nameOfVis) {
-    	this.Depth = dep;
-    	this.NameOfCoach = name;
-    	this.Volume = vol;
-    	this.MaxAmoutOfVisitors = amountOfVis;
-    	this.Address = adr;
-    	this.Width = wid;
-    	this.NameOfVisitor = nameOfVis;
+    public void resetValues(int dep, String name,
+    		int vol, int amountOfVis,
+    		String adr, int wid,
+    		String nameOfVis) {
+    	this.depth = dep;
+    	this.nameOfCoach = name;
+    	this.volume = vol;
+    	this.maxAmoutOfVisitors = amountOfVis;
+    	this.address = adr;
+    	this.width = wid;
+    	this.nameOfVisitor = nameOfVis;
+    	return;
     }
 }
